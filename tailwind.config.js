156447d2258e10d2ffe326851a0133c4384a2c81
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2025-01-03 23:34:55 +0600

    added custom colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        default: "#ff8c00",
+        light: "#ffa94d",
+        dark: "#cc6e00",
+      },
+    },
   },
   plugins: [daisyui],
 };
